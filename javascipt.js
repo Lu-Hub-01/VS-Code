bd8a1eb9a41ed4eb5ae28a081d090b9da2e752e8
Lu-Hub-01 <MellowScripts@outlook.com>
2026-08-19 02:23:33 +0200

    last chapter...maybe not

--- javascipt.js
+++
@@ -1,7 +0,0 @@
-const subscribeButton = document.getElementById('subscribeButton');
-
-if (subscribeButton) {
-    subscribeButton.addEventListener('click', () => {
-        window.location.href = 'mainPage.html';
-    });
-}
